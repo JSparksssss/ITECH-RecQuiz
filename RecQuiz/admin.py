37dd0dd7686eb1632d3927df185072881b2e1316
Yusheng Fan <2660781F@student.gla.ac.uk>
2022-03-03 18:12:03 +0000

    'Model'

--- RecQuiz/admin.py
+++ RecQuiz/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from RecQuiz.models import User, Course, Lecture, Quiz
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id','psw','email_id','first_name','last_name','gender','phone_number')
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_id','coordinator','course_name')
+
+
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Course,CourseAdmin)
+admin.site.register(Lecture)
+admin.site.register(Quiz)
 # Register your models here.
