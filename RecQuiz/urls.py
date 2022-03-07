9c37049f23f55e322cfaf65327b9fb392cb3576c
JSparksssss <37327269+JSparksssss@users.noreply.github.com>
2022-03-07 01:50:10 +0000

    Update Template

--- RecQuiz/urls.py
+++ RecQuiz/urls.py
@@ -5,10 +5,15 @@ app_name = 'RecQuiz'
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
     path('login/', views.login, name='login'),
     path('my_course/',views.my_course,name="my_course"),
-    path('new_course/',views.new_course_page,name="new_course_page"),
+    path('courses/',views.courses,name="courses"),
     path('add_course/',views.add_course,name="add_course"),
     path('course/<slug:course_name_slug>/',views.course,name="course"),
     path('register/',views.register,name="register"),
+    path('contact/',views.contact,name="contact"),
+    path('pricing/',views.pricing,name="pricing"),
+    path('trainers/',views.trainers,name="trainers"),
+    path('events/',views.events,name="events")
 ]
--- RecQuiz/views.py
+++ RecQuiz/views.py
@@ -1,15 +1,15 @@
 from difflib import context_diff
 from django.http import HttpResponse
 from django.shortcuts import render
-from RecQuiz.models import Course, Lecture,User
+from RecQuiz.models import Course, Lecture,Quiz,User
 # Create your views here.
 def index(request):
     #这是一个做判断的页面
     #假设用户已经登陆网页了，那么重定向到myCourse页面；如果用户没有登陆，那么重定向到login界面
     return render(request,'RecQuiz/index.html')
-    
+
 def login(request):
-    return HttpResponse("This is login page.")
+    return render(request,'RecQuiz/login.html')
 
 def my_course(request):
     context_dict = {}
@@ -24,7 +24,7 @@ def my_course(request):
     return render(request, 'RecQuiz/my_course.html', context = context_dict)
 
 
-def new_course_page(request):
+def courses(request):
     #应该能够筛选出用户未注册的course
     context_dict = {}
     try:
@@ -35,7 +35,7 @@ def new_course_page(request):
     except Course.DoesNotExist:
         context_dict['course'] = None
     
-    return render(request,'RecQuiz/new_course.html',context = context_dict)
+    return render(request,'RecQuiz/courses.html',context = context_dict)
 
 def add_course(request):
     if request.method == 'POST':
@@ -48,16 +48,32 @@ def course(request,course_name_slug):
     try:
         course = Course.objects.get(slug = course_name_slug)
         lectures = Lecture.objects.filter(course = course)
+        quizs = Quiz.objects.all()
         context_dict['course'] = course
         context_dict['lecutres'] = lectures
+        context_dict['quizs'] = quizs
     except:
-        context_dict['course'] = course
-        context_dict['lecutres'] = lectures
+        context_dict['course'] = None
+        context_dict['lecutres'] = None
+        context_dict['quizs'] = None
     return render(request,'RecQuiz/course.html',context = context_dict)
 
 def register(request):
     return HttpResponse("This is register page.")
    
+def about(request):
+    return render(request,'RecQuiz/about.html')
 
+def contact(request):
+    return render(request,'RecQuiz/contact.html')
+
+def pricing(request):
+    return render(request,'RecQuiz/pricing.html')
+
+def trainers(request):
+    return render(request,'RecQuiz/trainers.html')
+    
+def events(request):
+    return render(request,'RecQuiz/events.html')
 #TBC...
 #Quiz Report, and other functionalities
