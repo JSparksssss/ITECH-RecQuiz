22736d974e21436d2ce5f0e5d9bf7f6f14f0241a
JSparksssss <37327269+JSparksssss@users.noreply.github.com>
2022-03-15 22:53:24 +0000

    Log in/out success

--- static/js/base.js
+++ static/js/base.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
     var is_login = getCookie("is_login")
     console.log(is_login)
     if(is_login == "True"){
-        var logged = $('<a href="{% url '+ 'RecQuiz:logout' + ' %}" class="get-started-btn">Log Out</a>');
+        var logged = $('<a href="{% url 'RecQuiz:logout' %}" class="get-started-btn">Log Out</a>');
         $("#navbar").after(logged);
     }else{
-        var passenger = $('<a href="{% url '+'RecQuiz:login'+' %}" class="get-started-btn">Get Started</a>');
+        var passenger = $('<a href="{% url 'RecQuiz:login' %}" class="get-started-btn">Get Started</a>');
         $("#navbar").after(passenger);
     }
 })
