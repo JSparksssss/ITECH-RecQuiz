ef56b00f9dec8a63cba613432930ae06fce56fe1
JSparksssss <37327269+JSparksssss@users.noreply.github.com>
2022-03-17 17:15:11 +0000

    Display quiz

--- static/assets/js/marker-quiz.js
+++ static/assets/js/marker-quiz.js
@@ -1,48 +1,43 @@
- // initialize video.js
+ //initialize video.js
 
- var video = videojs('my_video');
+  var video = videojs('my_video');
 
- 
- var jsonData = JSON.parse(document.querySelector('#jsonData').getAttribute('data-json'));
- console.log("jsonData",jsonData);
- console.log(jsonData.length)
+ //import jsonData from django view
+  var questions = JSON.parse(document.querySelector('#jsonData').getAttribute('data-json'));
+  console.log("questions:",questions);
 
- var questions = jsonData
- console.log("questions:",questions)
-//  var questions = [{
-//     question: "What is your major?",
-//     choices: ['Internet Technology','Computing Science','Data Science','Software Development'],
-//     correctAnswer: 'Internet Technology'
-//   }, {
-//     question: "What is your major?",
-//     choices: ['Internet Technology','Computing Science','Data Science','Software Development'],
-//     correctAnswer: 'Computing Science'
-//   }];
-  
+  //create a question_list for loading quiz list
+  var question_list = document.getElementsByClassName('question-list-item');
+
+  //Click handler for the question_list_item
+  for(var i = 0; i < question_list.length; i++){
+    $('.question-list-item').eq(i).on('click',function(e){
+      console.log("Question box is clicking.")
+      //Create a modal      
+    })
+  }
+
   var questionCounter = 0; //Tracks question number
   var selections = []; //Array containing user choices
   var quiz = $('#quiz'); //Quiz div object
   
+  //initialize markers 
   var markers = []
 
-  for (var i = 0; i < jsonData.length; i++){
-    marker = {
-      time:jsonData[i].time,
-      text:jsonData[i].question
+  for (var i = 0; i < questions.length; i++){
+    var marker = {
+      time:questions[i].time,
+      text:questions[i].question
     }
     markers.push(marker)
   }
   console.log("markers:",markers)
- console.log("marker-quiz.js is loading");
- //quiz可以先通过html页面存放到cookie中
- //记得在跳出页面时消除cookie
-
+  console.log("marker-quiz.js is loading");
 
  //load the marker plugin
  video.markers({
    //When reach the markers, load the quiz page
    onMarkerReached: function(marker) {
-       
      $('#quiz-box').css('visibility', 'visible');
      video.pause()
      //Display questions
@@ -53,8 +48,8 @@
    markers: this.markers
  });
 
-  // Click handler for the 'next' button
-  $('#next').on('click', function (e) {
+  //click handler for the 'submit' button
+  $('#submit').on('click', function (e) {
     e.preventDefault();
     
     // Suspend click listener during fade animation
@@ -77,38 +72,43 @@
         else{
             alert(`Your answer is wrong. The correct Answer is ${questions[questionCounter].correctAnswer}`)
         }
+        //Display the button
+        $('.question-list-item').eq(questionCounter).attr('disabled', false);
         questionCounter++;
         video.play()
+
       //After finishing a question, hide the quesition box
       $('#quiz-box').css('visibility', 'hidden');
+
     }
   });
   
-  // Click handler for the 'prev' button
-  $('#prev').on('click', function (e) {
-    e.preventDefault();
+  // //Click handler for the 'prev' button
+  // $('#prev').on('click', function (e) {
+  //   e.preventDefault();
     
-    if(quiz.is(':animated')) {
-      return false;
-    }
-    choose();
-    questionCounter--;
-    displayNext();
-  });
+  //   if(quiz.is(':animated')) {
+  //     return false;
+  //   }
+  //   choose();
+  //   questionCounter--;
+  //   displayNext();
+  // });
   
-  // Click handler for the 'Start Over' button
-  $('#start').on('click', function (e) {
-    e.preventDefault();
+  // //Click handler for the 'Start Over' button
+  // $('#start').on('click', function (e) {
+  //   e.preventDefault();
     
-    if(quiz.is(':animated')) {
-      return false;
-    }
-    questionCounter = 0;
-    selections = [];
-    displayNext();
-    $('#start').hide();
-  });
+  //   if(quiz.is(':animated')) {
+  //     return false;
+  //   }
+  //   questionCounter = 0;
+  //   selections = [];
+  //   displayNext();
+  //   $('#start').hide();
+  // });
   
+
   // Animates buttons on hover
   $('.button').on('mouseenter', function () {
     $(this).addClass('active');
@@ -117,8 +117,8 @@
     $(this).removeClass('active');
   });
   
-  // Creates and returns the div that contains the questions and 
-  // the answer selections
+  //Creates and returns the div that contains the questions and 
+  //the answer selections
   function createQuestionElement(index) {
     var qElement = $('<div>', {
       id: 'question'
@@ -136,7 +136,7 @@
     return qElement;
   }
   
-  // Creates a list of the answer choices as radio inputs
+  //Creates a list of the answer choices as radio inputs
   function createRadios(index) {
     var radioList = $('<ul>');
     var item;
@@ -151,12 +151,12 @@
     return radioList;
   }
   
-  // Reads the user selection and pushes the value to an array
+  //Reads the user selection and pushes the value to an array
   function choose() {
     selections[questionCounter] = +$('input[name="answer"]:checked').val();
   }
   
-  // Displays next requested element
+  //Displays next requested element
   function displayNext() {
     quiz.fadeOut(function() {
       $('#question').remove();
@@ -174,19 +174,19 @@
         } else if(questionCounter === 0){
           
           $('#prev').hide();
-          $('#next').show();
+          $('#submit').show();
         }
       }else {
         var scoreElem = displayScore();
         quiz.append(scoreElem).fadeIn();
-        $('#next').hide();
+        $('#submit').hide();
         $('#prev').hide();
         $('#start').show();
       }
     });
   }
   
-  // Computes score and returns a paragraph element to be displayed
+  //Computes score and returns a paragraph element to be displayed
   function displayScore() {
     var score = $('<p>',{id: 'question'});
     
@@ -201,3 +201,7 @@
                  questions.length + ' right!!!');
     return score;
   }
+
+
+
+  
