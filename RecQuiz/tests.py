7c1d522776783d75a90ed0d4c91ff2b51e61edbc
AmoyMk <97642480+AmoyMk@users.noreply.github.com>
2022-03-18 23:43:44 +0000

    Update tests.py

--- RecQuiz/tests.py
+++ RecQuiz/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class RemoveCourse(TestCase):
-    def test_remove_course(self):
-        response = self.client.get(reverse('RecQuiz:remove_course'))
+class RemoveCourseTests(TestCase):
+    def test_checkWeb(self):
+        response = self.client.get(reverse('RecQuiz:my_course'))
         self.assertEqual(response.status_code,200)
